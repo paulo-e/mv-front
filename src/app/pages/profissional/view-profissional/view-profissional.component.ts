c754c519c676cfefce9eaec853cd7af552f1a04a
Paulo Elienay <paulo@pauloelienay.com>
2021-09-03 12:59:34 -0300

    feat(Profissional): implements view component

--- src/app/pages/profissional/view-profissional/view-profissional.component.ts
+++ src/app/pages/profissional/view-profissional/view-profissional.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Profissional } from 'src/app/model/profissional.model';
+import { ProfissionalService } from 'src/app/service/profissional.service';
+import { ToastUtilService } from 'src/app/service/toast-util.service';
 
 @Component({
   selector: 'app-view-profissional',
   templateUrl: './view-profissional.component.html',
-  styleUrls: ['./view-profissional.component.scss']
+  styleUrls: ['./view-profissional.component.scss'],
 })
 export class ViewProfissionalComponent implements OnInit {
+  profissional: Profissional;
+  loading: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private service: ProfissionalService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastUtil: ToastUtilService
+  ) {
+    this.loading = true;
+    this.profissional = new Profissional();
+    this.setProfissionalByUrlParam();
   }
 
+  setProfissionalByUrlParam() {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.service.getProfissionalById(id).subscribe(
+      (res) => {
+        this.profissional = res;
+      },
+      (err) => {
+        this.toastUtil.showError(err);
+      }
+    );
+    this.loading = false;
+  }
+
+  goBack() {
+    this.router.navigateByUrl('profissionais');
+  }
+
+  ngOnInit(): void {}
 }
